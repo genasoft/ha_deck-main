c397d098860f1e7b3cf65257972e7cc63bb205e7
genasoft <42194786+genasoft@users.noreply.github.com>
2026-03-19 10:44:14 -0400

    Change bus_parallel8 to Bus_Parallel8 in LGFX.h

--- components/hd_device_sc01_plus/LGFX.h
+++ components/hd_device_sc01_plus/LGFX.h
@@ -2,6 +2,7 @@
 
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
+#include <lgfx/v1/platforms/esp32/Bus_Parallel8.hpp>
 
 #define TFT_WIDTH 320
 #define TFT_HEIGHT 480
@@ -12,7 +13,7 @@ namespace hd_device {
 class LGFX : public lgfx::LGFX_Device
 {
   lgfx::v1::Panel_ST7796 _panel_instance;
-  lgfx::v1::bus_parallel8 _bus_instance;
+  lgfx::v1::Bus_Parallel8 _bus_instance;
   lgfx::v1::Light_PWM _light_instance;
   lgfx::v1::Touch_FT5x06 _touch_instance;
 
