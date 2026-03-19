5326030389afbbb00af9153f8c38b45736dae079
genasoft <42194786+genasoft@users.noreply.github.com>
2026-03-19 09:51:42 -0400

    Update LovyanGFX include and namespace usage

--- components/hd_device_sc01_plus/LGFX.h
+++ components/hd_device_sc01_plus/LGFX.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LGFX_USE_V1
-#include <LovyanGFX.h>
+#include <LovyanGFX.hpp>
 
 #define TFT_WIDTH 320
 #define TFT_HEIGHT 480
@@ -11,10 +11,10 @@ namespace hd_device {
 
 class LGFX : public lgfx::LGFX_Device
 {
-  lgfx::Panel_ST7796 _panel_instance;
-  lgfx::Bus_Parallel8 _bus_instance;
-  lgfx::Light_PWM _light_instance;
-  lgfx::Touch_FT5x06 _touch_instance;
+  lgfx::v1::Panel_ST7796 _panel_instance;
+  lgfx::v1::Bus_Parallel8 _bus_instance;
+  lgfx::v1::Light_PWM _light_instance;
+  lgfx::v1::Touch_FT5x06 _touch_instance;
 
 public:
   LGFX(void);
